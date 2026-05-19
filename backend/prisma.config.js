e0b8fa9a3895412433d32bd0b7c3a10e3876627c
Rusu Iulian <iulianrusu29@gmail.com>
2026-05-19 09:05:10 +0300

    try to deploy 3

--- backend/prisma.config.js
+++ backend/prisma.config.js
@@ -1,15 +1,8 @@
-const { PrismaPg } = require('@prisma/adapter-pg')
-const { Pool } = require('pg')
 const { defineConfig } = require('prisma/config')
 
 module.exports = defineConfig({
   schema: 'prisma/schema.prisma',
   migrate: {
-    adapter() {
-      const pool = new Pool({ 
-        connectionString: process.env.DATABASE_URL 
-      })
-      return new PrismaPg(pool)
-    },
-  },
+    url: process.env.DATABASE_URL
+  }
 })
